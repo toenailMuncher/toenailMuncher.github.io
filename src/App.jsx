b9e16625b8c402698bcc7ca7f4d9ed9330220b8a
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-01-20 23:22:17 -0500

    GRAH

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    
-    <div>
-       Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
