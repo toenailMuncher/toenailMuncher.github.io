f6dc1cb6f831def681460a8168cdf4e684c621e9
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-01-20 23:36:39 -0500

    trying to add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import {navLinks } from '../constants'
+import {logo, menu, close} from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav
+      className={''}'>
+      
+      
+      
+    </nav>
   )
 }
 
