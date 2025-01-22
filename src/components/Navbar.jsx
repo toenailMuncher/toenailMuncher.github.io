f8a27991de96da8acd01faadb81b9e7380bca48d
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2025-01-21 22:48:15 -0500

    added link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import {styles} from '../styles';
-import {navLinks} from '../constants';
-import {logo, menu, close} from '../assets';
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-const [active, setActive] = useState('');
+  const [active, setActive] = useState('');
 
   return (
-    <nav className={'${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}>
-    
-    <div className = "w-full flex justify-between items-center max-w-7xl mx-auto"> 
-      <Link to ="/" className = "flex items-center gap-2" onClick={() => {setActive(""); window.scrollTo(0,0);}}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 
-      <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
 
-      </Link>
+        <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(''); window.scrollTo(0, 0); }}>
 
-    </div>
+          <img src={logo} alt="logo" style={{ width: '250px', height: '200px' }} className="object-contain" />
 
+          <p className="text-white text-[18px] font-bold cursor-pointer">Marcus <span className="sm:block hidden"> Chuong</span> </p>
+          
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
